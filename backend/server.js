3e750df9b1c109ed6b05da6bedf8a97630f4833c
Radiant19 <152776230+Radiant19@users.noreply.github.com>
2025-04-24 09:18:18 +0530

    origin ch

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: [' http://localhost:3000'],
+    origin: ['http://localhost:3000'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
